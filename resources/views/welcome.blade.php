834bdabd7d95f736ef40f6680d84942cc93859ce
FJL <fabian.main@gmail.com>
2022-06-21 10:13:47 -0500

    print php version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,6 +83,9 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
+                <div class="title m-b-md">
+                    php {{ phpversion() }}
+                </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
